c43bc3ff7d96a9d4722e63d2098a8455237586d7
kubedav <davesade.42@gmail.com>
2017-03-10 15:51:49 +0100

    File cleanup.

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ module.exports = botBuilder(function(request) {
             console.log(state);
             if (JSON.stringify(state) === '{}') {
             var message = 'Ahoj! My se jeste koukam nezname, ale to vubec nevadi! Zalozil jsem Ti novy seznam NAKUP (tak to delam pokazde, kdyz se s nekym seznamim). Muzes ho zacit plnit prikazem PRIDEJ. Pro napovedu staci napsat POMOC.';
-            console.log('AHA! NEW USER DETECTED!')
+            console.log('AHA! NEW USER DETECTED!');
             listOps.newUser(userId);
             done(message);
             }
